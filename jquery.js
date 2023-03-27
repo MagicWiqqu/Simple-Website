497b4ae9cdf1ee473dd03e77fa548717d7d729a5
Wiktoria Arendarczyk <wiktoria.arendarczyk@onet.pl>
2023-03-27 16:31:12 +0200

    side menu continuation

--- jquery.js
+++ jquery.js
@@ -16,6 +16,24 @@ $("#ToLore").click(function() {
     }, 2000);
 });
 
+function scroll1(){
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#sekcja1").offset().top - 35
+    }, 2000);
+}
+
+function scroll2(){
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#sekcja2").offset().top - 35
+    }, 2000);
+}
+
+function scroll3(){
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#sekcja3").offset().top - 35
+    }, 2000);
+}
+
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     var ograniczenie = $("#sekcja1").offset().top;
@@ -25,4 +43,21 @@ $(window).scroll(function() {
     if(scroll < ograniczenie){
         $("#side-contain").slideUp("slow");
     }
+});
+
+$("#song-on").hide();
+$("#song-off").show();
+$(document).ready(function(){
+    $("#song-change").click(function(){
+        var audio = $(".audio")[0];
+        if (audio.paused) {
+            audio.play();
+            $("#song-on").show();
+            $("#song-off").hide();
+        }  else {
+            audio.pause();
+            $("#song-on").hide();
+            $("#song-off").show();
+        }
+    });
 });
